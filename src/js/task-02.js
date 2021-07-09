c2280e745973d5c2af1195d0b446b912543891a2
Vadym Polishchuk <polischukvadim@gmail.com>
2021-07-09 23:29:23 +0300

    added second option

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -13,3 +13,12 @@ for (const ingredient of ingredients) {
 itemEl.textContent = ingredient;
 document.querySelector('#ingredients').appendChild(itemEl);
 }
+
+
+// ============= 2-й вариант =============
+
+  // const ingredientsList = ingredients.map(ingredient => {
+  //   const itemEl = document.createElement('li');
+  //   itemEl.textContent = ingredient;
+  //   console.log(document.querySelector('#ingredients').appendChild(itemEl));
+  // });
